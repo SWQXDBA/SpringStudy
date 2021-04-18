bd540476f263315bce2f7ac15f58741b070a7cd2
SWQXDBA <983110853@qq.com>
2021-04-18 23:00:02 +0800

    Default Changelist

--- jpa_mysql/src/test/java/com/example/demo/SpecTest.java
+++ jpa_mysql/src/test/java/com/example/demo/SpecTest.java
@@ -1,15 +1,13 @@
 package com.example.demo;
 
 import com.example.demo.Repositorys.UserDao;
+import com.example.demo.models.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.domain.Specification;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 
 @SpringBootTest
 public class SpecTest {
@@ -17,12 +15,21 @@ public class SpecTest {
     private UserDao userDao;
 
     @Test
+    //查找一个对象
     public void t1() {
-        Specification specification = new Specification() {
+        Specification<User> specification = new Specification<User>() {
             @Override
-            public Predicate toPredicate(Root root, CriteriaQuery criteriaQuery, CriteriaBuilder criteriaBuilder) {
-                return null;
+            public Predicate toPredicate(Root<User> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                //获取比较的属性
+                Path<Object> userName = root.get("name");
+                //equal精准匹配
+                Predicate p = criteriaBuilder.equal(userName, "name1");
+                return p;
             }
         };
+        //Optional的get（）返回这个对象
+        //直接返回Optional可以避免显示检测null
+        User user = userDao.findOne(specification).get();
+        System.out.println(user);
     }
 }
