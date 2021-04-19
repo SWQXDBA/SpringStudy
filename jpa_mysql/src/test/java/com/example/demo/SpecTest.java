0ad7a3e9d646b15daf5dad7f22d0b1e991b7b0d7
SWQXDBA <983110853@qq.com>
2021-04-19 09:50:14 +0800

    Default Changelist

--- jpa_mysql/src/test/java/com/example/demo/SpecTest.java
+++ jpa_mysql/src/test/java/com/example/demo/SpecTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.domain.Specification;
 
 import javax.persistence.criteria.*;
+import java.util.List;
 
 @SpringBootTest
 public class SpecTest {
@@ -31,5 +32,40 @@ public class SpecTest {
         //直接返回Optional可以避免显示检测null
         User user = userDao.findOne(specification).get();
         System.out.println(user);
+    }
+
+    @Test
+    //多条件查询
+    public void t2() {
+        Specification<User> specification = new Specification<User>() {
+            @Override
+            public Predicate toPredicate(Root<User> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                Path<Object> name = root.get("name");
+                Path<Object> email = root.get("email");
+                Predicate predicate = criteriaBuilder.equal(name, "name1");
+                Predicate predicate2 = criteriaBuilder.equal(email, "email1");
+                //两个条件同时成立用and
+                Predicate ret = criteriaBuilder.and(predicate, predicate2);
+                return ret;
+            }
+        };
+        User user = userDao.findOne(specification).get();
+        System.out.println(user);
+    }
+
+    @Test
+    //模糊匹配
+    public void t3() {
+        Specification<User> specification = new Specification<User>() {
+            @Override
+            public Predicate toPredicate(Root<User> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                Path<Object> path = root.get("name");
+                Predicate predicate = criteriaBuilder.like(path.as(String.class), "%name%");
+                return predicate;
+
+            }
+        };
+        List<User> users = userDao.findAll(specification);
+        System.out.println(users);
     }
 }
